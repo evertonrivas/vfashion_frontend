74ae5bd14a2973ffbfcdb1a0098d19f27846e507
evertonrivas <evertonrivas@gmail.com>
2023-10-10 22:36:00 -0300

    Flimv Configuration

--- src/app/models/system.enum.ts
+++ src/app/models/system.enum.ts
@@ -46,3 +46,11 @@ export enum DataSearch{
 export enum CustomerAction{
     
 }
+
+export enum CustomerCurve{
+    FIVE_STARS  = 5,
+    FOUR_STARS  = 4,
+    THREE_STARS = 3,
+    TWO_STARS   = 2,
+    ONE_STAR    = 1
+}
