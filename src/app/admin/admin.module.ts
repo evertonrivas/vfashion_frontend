b96034719e6c2c5e9560fddefe72954bc6481c83
evertonrivas <evertonrivas@gmail.com>
2023-10-10 22:32:56 -0300

    New component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { SharedModule } from '../shared/shared.module';
+import { SidebarModule } from 'primeng/sidebar';
+import { AppMenuitemComponent } from './app.menuitem.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    AppMenuitemComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    SharedModule
+    SharedModule,
+    SidebarModule
   ]
 })
 export class AdminModule { }
---
+++ src/app/admin/app.menuitem.component.ts
@@ -0,0 +1,91 @@
+import { ChangeDetectorRef, Component, HostBinding, Input, OnDestroy, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+@Component({
+    selector: '[app-menuitem]',
+    template: `<ng-container>
+        <div *ngIf="root && item.visible !== false" class="layout-menuitem-root-text">{{item.label}}</div>
+        <a *ngIf="(!item.routerLink || item.items) && item.visible !== false" [attr.href]="item.url" (click)="itemClick($event)"
+        [ngClass]="item.class" [attr.target]="item.target" tabindex="0" pRipple>
+            <i [ngClass]="item.icon" class="layout-menuitem-icon"></i>
+            <span class="layout-menuitem-text">{{item.label}}</span>
+            <i class="pi pi-fw pi-angle-down layout-submenu-toggler" *ngIf="item.items"></i>
+        </a>
+        <a *ngIf="(item.routerLink && !item.items) && item.visible !== false" (click)="itemClick($event)" [ngClass]="item.class" 
+        [routerLink]="item.routerLink" routerLinkActive="active-route" [routerLinkActiveOptions]="item.routerLinkActiveOptions||{ paths: 'exact', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' }"
+        [fragment]="item.fragment" [queryParamsHandling]="item.queryParamsHandling" [preserveFragment]="item.preserveFragment" 
+        [skipLocationChange]="item.skipLocationChange" [replaceUrl]="item.replaceUrl" [state]="item.state" [queryParams]="item.queryParams"
+        [attr.target]="item.target" tabindex="0" pRipple>
+            <i [ngClass]="item.icon" class="layout-menuitem-icon"></i>
+            <span class="layout-menuitem-text">{{item.label}}</span>
+            <i class="pi pi-fw pi-angle-down layout-submenu-toggler" *ngIf="item.items"></i>
+        </a>
+
+        <ul *ngIf="item.items && item.visible !== false" [@children]="submenuAnimation">
+            <ng-template ngFor let-child let-i="index" [ngForOf]="item.items">
+                <li app-menuitem [item]="child" [index]="i" [parentKey]="key" [class]="child.badgeClass"></li>
+            </ng-template>
+        </ul>
+    </ng-container>`,
+    animations: [
+        trigger('children', [
+            state('collapsed', style({
+                height: '0'
+            })),
+            state('expanded', style({
+                height: '*'
+            })),
+            transition('collapsed <=> expanded', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
+        ])
+    ]
+})
+export class AppMenuitemComponent implements OnInit{
+    @Input() item:any;
+
+    @Input() index!: number;
+    @Input() @HostBinding('class.layout-root-menuitem') root!: boolean;
+    @Input() parentKey!: string;
+    key: string = "";
+    active:boolean = false;
+
+    constructor(private cd: ChangeDetectorRef, public router: Router){
+        
+    }
+
+    ngOnInit(): void {
+        this.key = this.parentKey ? this.parentKey + '-' + this.index : String(this.index);
+
+        if (this.item.routerLink) {
+            this.updateActiveStateFromRoute();
+        }
+        this.cd.detectChanges();
+    }
+
+    updateActiveStateFromRoute() {
+        let activeRoute = this.router.isActive(this.item.routerLink[0], { paths: 'exact', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' });
+    }
+
+    itemClick(event: Event) {
+        // avoid processing disabled items
+        if (this.item.disabled) {
+            event.preventDefault();
+            return;
+        }
+
+        // execute command
+        if (this.item.command) {
+            this.item.command({ originalEvent: event, item: this.item });
+        }
+
+        // toggle active state
+        if (this.item.items) {
+            this.active = !this.active;
+        }
+    }
+
+    get submenuAnimation() {
+        return this.root ? 'expanded' : (this.active ? 'expanded' : 'collapsed');
+    }
+    
+}
