46f272211b5c24d6fa3b6dd3b32a53c5c50bd18d
evertonrivas <evertonrivas@gmail.com>
2023-10-12 10:53:02 -0300

    Added options as parameter

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { ProductCategory, ProductCollection, B2bBrand, ProductType, ProductModel, Size, Color } from 'src/app/models/product.model';
 import { Filter } from '../models/filter.model';
 import { MyHttp } from './my-http';
+import { Options, RequestResponse, ResponseError } from '../models/paginate.model';
 
 @Injectable({
   providedIn: 'root'
@@ -21,52 +22,59 @@ export class FilterService extends MyHttp{
     this.filterAnnouced.next(filters);
   }
 
-  listBrand():Observable<B2bBrand[]>{ 
-    return this.http.get<B2bBrand[]>(this.sys_config.backend_b2b+'/brand/',{
-      params: this.getListAll(false,"name"),
-      headers: this.getHeader()
+  listBrand(opt:Options):Observable<B2bBrand[]|RequestResponse|ResponseError>{ 
+    return this.http.get<B2bBrand[]|RequestResponse|ResponseError>(this.sys_config.backend_b2b+'/brand/',{
+      headers: this.getHeader(),
+      params: new HttpParams().set("page",opt.page)
+        .set("pageSize",opt.pageSize).set("query",opt.query)
     });
   }
 
-  listCollection():Observable<ProductCollection[]>{
-    return this.http.get<ProductCollection[]>(this.sys_config.backend_b2b+'/collection/',{
-      params: this.getListAll(),
-      headers: this.getHeader()
+  listCollection(opt:Options):Observable<ProductCollection[]|RequestResponse|ResponseError>{
+    return this.http.get<ProductCollection[]|RequestResponse|ResponseError>(this.sys_config.backend_b2b+'/collection/',{
+      headers: this.getHeader(),
+      params: new HttpParams().set("page",opt.page)
+        .set("pageSize",opt.pageSize).set("query",opt.query)
     });
   }
 
-  listCategory(){
-    return this.http.get<ProductCategory[]>(this.sys_config.backend_cmm+'/products-category/',{
-      params: this.getListAll(false,"name"),
-      headers: this.getHeader()
+  listCategory(opt:Options):Observable<ProductCategory[]|RequestResponse|ResponseError>{
+    return this.http.get<ProductCategory[]|RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/products-category/',{
+      headers: this.getHeader(),
+      params: new HttpParams().set("page",opt.page)
+        .set("pageSize",opt.pageSize).set("query",opt.query)
     });
   }
 
-  listType():Observable<ProductType[]>{
-    return this.http.get<ProductType[]>(this.sys_config.backend_cmm+'/products-type/',{
-      params: this.getListAll(false,"name"),
-      headers: this.getHeader()
+  listType(opt:Options):Observable<ProductType[]|RequestResponse|ResponseError>{
+    return this.http.get<ProductType[]|RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/products-type/',{
+      headers: this.getHeader(),
+      params: new HttpParams().set("page",opt.page)
+        .set("pageSize",opt.pageSize).set("query",opt.query)
     });
   }
 
-  listModel():Observable<ProductModel[]>{
-    return this.http.get<ProductModel[]>(this.sys_config.backend_cmm+'/products-model/',{
-      params: this.getListAll(false,"name"),
-      headers: this.getHeader()
+  listModel(opt:Options):Observable<ProductModel[]|RequestResponse|ResponseError>{
+    return this.http.get<ProductModel[]|RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/products-model/',{
+      headers: this.getHeader(),
+      params: new HttpParams().set("page",opt.page)
+        .set("pageSize",opt.pageSize).set("query",opt.query)
     });
   }
 
-  listSize(){
-    return this.http.get<Size[]>(this.sys_config.backend_cmm+'/translate-sizes/',{
-      params: this.getListAll(),
-      headers: this.getHeader()
+  listSize(opt:Options):Observable<Size[]|RequestResponse|ResponseError>{
+    return this.http.get<Size[]|RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/translate-sizes/',{
+      headers: this.getHeader(),
+      params: new HttpParams().set("page",opt.page)
+        .set("pageSize",opt.pageSize).set("query",opt.query)
     });
   }
 
-  listColor(){
-    return this.http.get<Color[]>(this.sys_config.backend_cmm+'/translate-colors/',{
-      params: this.getListAll(false,"name"),
-      headers: this.getHeader()
+  listColor(opt:Options):Observable<Color[]|RequestResponse|ResponseError>{
+    return this.http.get<Color[]|RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/translate-colors/',{
+      headers: this.getHeader(),
+      params: new HttpParams().set("page",opt.page)
+        .set("pageSize",opt.pageSize).set("query",opt.query)
     });
   }
 }
