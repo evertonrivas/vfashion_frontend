9c750bc131072b78c405e44d0d67fcca4a899141
evertonrivas <evertonrivas@gmail.com>
2023-10-18 19:34:34 -0300

    New modules routes

--- src/app/salesforce/salesforce-routing.module.ts
+++ src/app/salesforce/salesforce-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SalesforceComponent } from './salesforce.component';
 import { ProfileComponent } from '../common/profile/profile.component';
+import { GridComponent } from './grid/grid.component';
+import { HistoryComponent } from './history/history.component';
+import { OrderComponent } from './order/order.component';
+import { ReturnComponent } from './return/return.component';
 
 const routes: Routes = [{ 
   path: '', 
   component: SalesforceComponent,
-  children:[{
-    path: 'profile',
-    component: ProfileComponent
-  }]
+  children:[{ path: 'profile', component: ProfileComponent },
+  { path: 'grid', component: GridComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'order', component: OrderComponent },
+  { path: 'return', component: ReturnComponent }]
 }];
 
 @NgModule({
