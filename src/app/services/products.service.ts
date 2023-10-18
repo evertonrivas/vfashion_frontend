43cad8c248d9752717ecbf8291078cd10aa91be8
evertonrivas <evertonrivas@gmail.com>
2023-10-18 19:34:51 -0300

    listGallery corrections

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -13,47 +13,8 @@ export class ProductsService extends MyHttp{
     super(http);
   }
 
-  list(options:Options):Observable<RequestResponse|ResponseError>{
-    // let httpParams = new HttpParams()
-    // if (options.page!=null){
-    //   httpParams = httpParams.set("page",String(options.page));
-    // }
-    // if (options.pagSize!=null){
-    //   httpParams = httpParams.set("pageSize",String(options.pagSize));
-    // }
-    // if (options.search!=""){
-    //   httpParams = httpParams.set("query",String(options.search));
-    // }
-    // if (options.orderBy!=null){
-    //   httpParams = httpParams.set("order_by",String(options.orderBy))
-    //   .set("order_dir",options.orderDir);
-    // }
-    // if (String(options.brand)!=""){
-    //   httpParams = httpParams.set('brand',String(options.brand))
-    // }
-    // if (String(options.collection)!=""){
-    //   httpParams = httpParams.set('collection',String(options.collection));
-    // }
-    // if (String(options.category)!=""){
-    //   httpParams = httpParams.set('category',String(options.category));
-    // }
-    // if (String(options.model)!=""){
-    //   httpParams = httpParams.set('model',String(options.model));
-    // }
-    // if (String(options.type)!=""){
-    //   httpParams = httpParams.set('type',String(options.type));
-    // }
-    // if (String(options.color)!=""){
-    //   httpParams = httpParams.set('color',String(options.color));
-    // }
-    // if (String(options.size)!=""){
-    //   httpParams = httpParams.set('size',String(options.size));
-    // }
-    // if (String(options.search!="")){
-    //   httpParams = httpParams.set('query',String(options.search));
-    // }
-
-    return this.http.get<RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/products/gallery/',{
+  listGallery(options:Options):Observable<ProductStock[]|RequestResponse|ResponseError>{
+    return this.http.get<ProductStock[]|RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/products/gallery/',{
       headers: this.getHeader(),
       params: new HttpParams().set("page",options.page).set("pageSize",options.pageSize).set("query",options.query)
     });
