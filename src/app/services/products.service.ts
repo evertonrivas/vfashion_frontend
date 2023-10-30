26b0c3e58fb6df7443926ece8e5ceb20d2f1c406
evertonrivas <evertonrivas@gmail.com>
2023-10-29 22:56:03 -0300

    api call changed

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ProductStock, Product } from 'src/app/models/product.model';
+import { Product } from 'src/app/models/product.model';
 import { MyHttp } from './my-http';
 import { Options, RequestResponse, ResponseError } from '../models/paginate.model';
 
@@ -13,15 +13,15 @@ export class ProductsService extends MyHttp{
     super(http);
   }
 
-  listGallery(options:Options):Observable<ProductStock[]|RequestResponse|ResponseError>{
-    return this.http.get<ProductStock[]|RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/products/gallery/',{
+  listGallery(options:Options):Observable<Product[]|RequestResponse|ResponseError>{
+    return this.http.get<Product[]|RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/products/gallery/',{
       headers: this.getHeader(),
       params: new HttpParams().set("page",options.page).set("pageSize",options.pageSize).set("query",options.query)
     });
   }
   
-  get_stock(id_product:number):Observable<ProductStock[]>{
-    return this.http.get<ProductStock[]>(this.sys_config.backend_b2b+'/product-stock/load-by-product/'+id_product,{
+  get_stock(id_product:number):Observable<Product[]>{
+    return this.http.get<Product[]>(this.sys_config.backend_b2b+'/product-stock/load-by-product/'+id_product,{
       headers: this.getHeader()
     });
   }
