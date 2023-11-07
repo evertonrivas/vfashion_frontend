fa88f38984dd2b4d397def3696c7f4224e91020a
evertonrivas <evertonrivas@gmail.com>
2023-11-07 09:26:59 -0300

    New Service Indicators

--- src/app/services/indicators.service.ts
+++ src/app/services/indicators.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { MyHttp } from './my-http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { ResponseError } from '../models/paginate.model';
+import { EntityType } from '../models/system.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -19,11 +20,39 @@ export class IndicatorsService extends MyHttp{
     this.counterAnnounced.next(null);  
   }
 
-  getCartTotal():Observable<number|ResponseError>{
+  b2bTotalCart():Observable<number|ResponseError>{
     return this.http.get<number|ResponseError>(this.sys_config.backend_b2b+'/cart/total/'+localStorage.getItem("id_profile"),{
       headers: this.getHeader()
     });
   }
 
+  //realiza a contagem total de entidades do calendario
+  calendarCountEntity(type:EntityType):Observable<number|ResponseError>{
+    return this.http.get<number|ResponseError>(this.sys_config.backend_cmm+'/legal-entities/count',{
+      headers: this.getHeader(),
+      params: new HttpParams().set("type",type.toString())
+    });
+   }
+
+   //realiza a contagem total de pedidos do calendario
+   calendarCountOrder():Observable<number|ResponseError>{
+    return this.http.get<number|ResponseError>(this.sys_config.backend_b2b+'/orders/total',{
+      headers: this.getHeader()
+    });
+   }
+
+   //realiza a soma total dos valores de pedidos do calendario
+   calendarValueOrder():Observable<number|ResponseError>{
+    return this.http.post<number|ResponseError>(this.sys_config.backend_b2b+'/orders/total',{
+      headers: this.getHeader()
+    });
+   }
+
+   //realiza a soma total dos pedidos por representante no calendario
+   calendarValueOrderByRepresentative():Observable<any>{
+    return this.http.get<any>(this.sys_config.backend_b2b+'',{
+      headers: this.getHeader()
+    });
+   }
   
 }
