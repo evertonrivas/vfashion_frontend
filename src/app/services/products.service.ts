170ecdba1931b35b5c2476605d7ce624b0775af4
evertonrivas <evertonrivas@gmail.com>
2023-11-08 15:42:33 -0300

    Methods listGaller and get_stok moved to b2b

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -13,19 +13,6 @@ export class ProductsService extends MyHttp{
     super(http);
   }
 
-  listGallery(options:Options):Observable<Product[]|RequestResponse|ResponseError>{
-    return this.http.get<Product[]|RequestResponse|ResponseError>(this.sys_config.backend_cmm+'/products/gallery/',{
-      headers: this.getHeader(),
-      params: new HttpParams().set("page",options.page).set("pageSize",options.pageSize).set("query",options.query)
-    });
-  }
-  
-  get_stock(id_product:number):Observable<Product[]>{
-    return this.http.get<Product[]>(this.sys_config.backend_b2b+'/product-stock/load-by-product/'+id_product,{
-      headers: this.getHeader()
-    });
-  }
-
   get(id_product:number):Observable<Product>{
     return this.http.get<Product>(this.sys_config.backend_cmm+'/products/'+id_product.toString(),{
       headers:this.getHeader()
