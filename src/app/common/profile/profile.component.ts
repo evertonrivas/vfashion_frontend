8f619af73b9e1534cea1b32cecbbfee470d25274
evertonrivas <evertonrivas@gmail.com>
2023-11-08 15:42:59 -0300

    Profile standalone to be constructed

--- src/app/common/profile/profile.component.ts
+++ src/app/common/profile/profile.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
   standalone:true,
+  imports:[
+    PanelModule,
+    ButtonModule,
+    InputTextModule,
+    InputNumberModule,
+    DropdownModule,
+    ToastModule
+  ],
+  providers:[
+    MessageService
+  ]
 })
 export class ProfileComponent {
 
