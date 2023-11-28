5721677bbf1e179117b334ed8a925f861e470d15
evertonrivas <evertonrivas@gmail.com>
2023-11-28 00:01:31 -0300

    history first version

--- src/app/salesforce/history/history.component.ts
+++ src/app/salesforce/history/history.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PaginatorState } from 'primeng/paginator';
 import { Common } from 'src/app/classes/common';
+import { OrderHistory } from 'src/app/models/order.model';
 import { RequestResponse } from 'src/app/models/paginate.model';
 import { B2bOrderService } from 'src/app/services/b2b.order.service';
 
@@ -10,11 +11,32 @@ import { B2bOrderService } from 'src/app/services/b2b.order.service';
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent extends Common{
-
+export class HistoryComponent extends Common implements AfterViewInit{
+  showIntegration:boolean = false;
+  showIntegrationTrack:boolean = false;
+  selectedOrder:OrderHistory = {
+    id_order: 0,
+    id_customer: 0,
+    customer_name: '',
+    id_payment_condition: 0,
+    payment_name: '',
+    total_value: 0,
+    total_itens: 0,
+    installments: 0,
+    installment_value: 0,
+    integrated: false,
+    integration_number: null,
+    invoice_number: null,
+    track: null,
+    date_created: ''
+  };
   constructor(route:Router,
     private svc:B2bOrderService){
     super(route);
+  }
+
+  ngAfterViewInit(): void {
+    this.loadHistory();
   }
 
   loadHistory(evt:PaginatorState = { page:0, pageCount:1 }):void{
@@ -22,7 +44,20 @@ export class HistoryComponent extends Common{
     this.svc.listMyOrders(this.options).subscribe({
       next: (data) =>{
         this.response = data as RequestResponse;
+        (this.response.data as OrderHistory[]).forEach((oh) =>{
+          if(oh.integrated==true){
+            this.showIntegration = true;
+            if(oh.track!=null){
+              this.showIntegrationTrack = true;
+            }
+          }
+        })
       }
     });
+  }
+
+  showTrack(p_idOrder:number):void{
+    this.selectedOrder = (this.response.data as OrderHistory[]).find((oh) => oh.id_order==p_idOrder) as OrderHistory;
+    this.showDialog = true;
   }
 }
