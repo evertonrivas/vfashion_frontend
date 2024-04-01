97c877a5467b2d2d5ffe1f10497dce06a347c22a
evertonrivas <evertonrivas@gmail.com>
2024-04-01 20:48:49 -0300

    Validation

--- src/app/common/form/form.component.ts
+++ src/app/common/form/form.component.ts
@@ -2,14 +2,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SharedModule } from '../shared.module';
 import { CommonModule } from '@angular/common';
 import { FieldCase, FieldType } from 'src/app/models/system.enum';
-import { FormRow } from 'src/app/models/field.model';
+import { FormField, FormRow } from 'src/app/models/field.model';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    PasswordModule
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
@@ -24,6 +26,7 @@ export class FormComponent {
   fieldType = FieldType;
   fieldCase = FieldCase;
   dataToSave:string = "";
+  sended:boolean = false;
 
   constructor(){
 
@@ -34,25 +37,44 @@ export class FormComponent {
   }
 
   doSave():void{
-    this.visible = false;
-    this.visibleChange.emit(this.visible);
+    this.sended = true;
 
-    this.dataToSave = '{"id":"'+this.registryId.toString()+'"';
-    this.rows.forEach((r) =>{
-      r.fields.forEach((f) =>{
-        if (f.case==FieldCase.UPPER){
-          this.dataToSave += ',"'+f.name+'":"'+String(f.value).toUpperCase()+'"'
-        }else if(f.case==FieldCase.LOWER){
-          this.dataToSave += ',"'+f.name+'":"'+String(f.value).toLowerCase()+'"'
-        }
-        else{
-          this.dataToSave += ',"'+f.name+'":"'+f.value+'"'
-        }
+    if(this.isValidated()){
+      this.visible = false;
+      this.visibleChange.emit(this.visible);
+
+      this.dataToSave = '{"id":"'+this.registryId.toString()+'"';
+      this.rows.forEach((r) =>{
+        r.fields.forEach((f) =>{
+          if (f.case==FieldCase.UPPER){
+            this.dataToSave += ',"'+f.name+'":"'+String(f.type==FieldType.COMBO?f.value.value:((f.type==FieldType.PASSWD?f.value[0]:f.value))).toUpperCase()+'"'
+          }else if(f.case==FieldCase.LOWER){
+            this.dataToSave += ',"'+f.name+'":"'+String(f.type==FieldType.COMBO?f.value.value:((f.type==FieldType.PASSWD?f.value[0]:f.value))).toLowerCase()+'"'
+          }
+          else{
+            this.dataToSave += ',"'+f.name+'":"'+(f.type==FieldType.COMBO?f.value.value:((f.type==FieldType.PASSWD?f.value[0]:f.value)))+'"'
+          }
+        });
       });
-    });
-    this.dataToSave += "}";
+      this.dataToSave += "}";
 
-    this.objectToSave.emit(JSON.parse(this.dataToSave));
+      this.objectToSave.emit(JSON.parse(this.dataToSave));
+    }
+  }
+
+  private isValidated():any{
+    for(var i=0;i<this.rows.length;i++){
+      for(var f=0;f<this.rows[i].fields.length;f++){
+        if(this.rows[i].fields[f].required==true && this.rows[i].fields[f].value==undefined && this.rows[i].fields[f].type!=this.fieldType.PASSWD){
+          return false;
+        }else if(this.rows[i].fields[f].type==FieldType.PASSWD){
+          if(this.rows[i].fields[f].required==true && (this.rows[i].fields[f].value[0]==undefined || this.rows[i].fields[f].value[1]==undefined) || (this.rows[i].fields[f].value[0]!=this.rows[i].fields[f].value[1])){
+            return false;
+          }
+        }
+      }
+    }
+    return true;
   }
 
   clearFields():void{
