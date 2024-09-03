0dbfb56af4e7aced5bfa4670d74bd8480009efb1
evertonrivas <evertonrivas@gmail.com>
2024-09-03 14:39:38 -0300

    new config flimv_model

--- src/app/classes/common.ts
+++ src/app/classes/common.ts
@@ -25,7 +25,8 @@ export class Common{
         company_max_up_files: 0,
         company_max_up_images: 0,
         company_use_url_images: false,
-        system_pagination_size: 0
+        system_pagination_size: 0,
+        flimv_model:""
     }
     module:ModuleName = ModuleName.NONE; // nome do module que estah em uso no sistema
     modulePath:string = ""; //url do caminho do modulo
@@ -39,6 +40,7 @@ export class Common{
     }; //opcoes padrao para buscas no sistema
     hasSended:boolean = false;//serve para validacao de campos quando ha envio
     hasSendDelete:boolean = false;//serve para validacao de campos quando ha exclusao
+    hasSendMassive:boolean = false; //serve para validacao de campos em acoes massivas
     serviceSub:Subscription[] = []; //armazena o retorno das subscriptions do sistema
     level_access!:AccessLevel; //nivel de acesso do usuario ao sistema
     levels = AccessLevel; //niveis de acesso existentes
@@ -69,7 +71,8 @@ export class Common{
             company_max_up_files: parseInt(JSON.stringify(localStorage.getItem("company_max_up_files")).replaceAll('"',"")),
             company_max_up_images: parseInt(JSON.stringify(localStorage.getItem("company_max_up_images")).replaceAll('"',"")),
             company_use_url_images: JSON.stringify(localStorage.getItem("company_use_url_images")).replaceAll('"',"")=="1"?true:false,
-            system_pagination_size: parseInt(JSON.stringify(localStorage.getItem("system_pagination_size")).replaceAll('"',""))
+            system_pagination_size: parseInt(JSON.stringify(localStorage.getItem("system_pagination_size")).replaceAll('"',"")),
+            flimv_model: JSON.stringify(localStorage.getItem("flimv_model")).replaceAll('"','')
         }
         this.options.pageSize = this.sysconfig.system_pagination_size;
 
