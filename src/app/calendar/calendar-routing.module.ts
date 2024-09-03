22d4ea8116f360376b4fa67be8a70ed57a77c34a
evertonrivas <evertonrivas@gmail.com>
2024-09-03 14:38:46 -0300

    Added report manager

--- src/app/calendar/calendar-routing.module.ts
+++ src/app/calendar/calendar-routing.module.ts
@@ -5,8 +5,9 @@ import { DashboardComponent } from '../common/dashboard/dashboard.component';
 import { GanttComponent } from './gantt/gantt.component';
 import { ProfileComponent } from '../common/profile/profile.component';
 import { FlimvComponent } from './flimv/flimv.component';
-import { ReportsComponent } from '../reports/reports.component';
 import { VersionComponent } from '../common/version/version.component';
+import { RepComissionComponent } from './rep-comission/rep-comission.component';
+import { ReportManagerComponent } from '../common/report-manager/report-manager.component';
 
 
 const routes: Routes = [{ 
@@ -18,7 +19,8 @@ const routes: Routes = [{
     { path: 'gantt', component: GanttComponent },
     { path: 'profile', component: ProfileComponent },
     { path:'flimv', component: FlimvComponent },
-    { path:'reports', component: ReportsComponent }
+    { path:'reports', component: ReportManagerComponent },
+    { path: 'comission', component: RepComissionComponent }
   ]
 }];
 
