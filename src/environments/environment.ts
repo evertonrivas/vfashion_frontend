b2c6ef8fd9ebcbb43b891d64fe43d5dc95286842
evertonrivas <evertonrivas@gmail.com>
2024-09-11 20:51:50 -0300

    new environment options

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,9 @@
 export const environment = {
-    // backend_b2b: "https://fast2bee.pythonanywhere.com/b2b/api",
-    // backend_fpr: "https://fast2bee.pythonanywhere.com/fpr/api",
-    // backend_crm: "https://fast2bee.pythonanywhere.com/crm/api",
-    // backend_scm: "https://fast2bee.pythonanywhere.com/scm/api",
-    // backend_cmm: "https://fast2bee.pythonanywhere.com/cmm/api",
-
-    backend_b2b: "https://fast2beezafira.pythonanywhere.com/b2b/api",
-    backend_fpr: "https://fast2beezafira.pythonanywhere.com/fpr/api",
-    backend_crm: "https://fast2beezafira.pythonanywhere.com/crm/api",
-    backend_scm: "https://fast2beezafira.pythonanywhere.com/scm/api",
-    backend_cmm: "https://fast2beezafira.pythonanywhere.com/cmm/api",
-
-    // backend_b2b: "https://fast2beedemo.pythonanywhere.com/b2b/api",
-    // backend_fpr: "https://fast2beedemo.pythonanywhere.com/fpr/api",
-    // backend_crm: "https://fast2beedemo.pythonanywhere.com/crm/api",
-    // backend_scm: "https://fast2beedemo.pythonanywhere.com/scm/api",
-    // backend_cmm: "https://fast2beedemo.pythonanywhere.com/cmm/api",
+    backend_b2b: "https://fast2bee.pythonanywhere.com/b2b/api",
+    backend_fpr: "https://fast2bee.pythonanywhere.com/fpr/api",
+    backend_crm: "https://fast2bee.pythonanywhere.com/crm/api",
+    backend_scm: "https://fast2bee.pythonanywhere.com/scm/api",
+    backend_cmm: "https://fast2bee.pythonanywhere.com/cmm/api",
     locale: {
         language: "pt",
         currency_code: "R$",
---
+++ src/environments/environment.zafira.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    backend_b2b: "https://fast2beezafira.pythonanywhere.com/b2b/api",
+    backend_fpr: "https://fast2beezafira.pythonanywhere.com/fpr/api",
+    backend_crm: "https://fast2beezafira.pythonanywhere.com/crm/api",
+    backend_scm: "https://fast2beezafira.pythonanywhere.com/scm/api",
+    backend_cmm: "https://fast2beezafira.pythonanywhere.com/cmm/api",
+    locale: {
+        language: "pt",
+        currency_code: "R$",
+        date_format: "shortDate",
+        timezone: "-0300"
+    },
+    recaptcha: {
+        siteKey : "6Le7VGUlAAAAAMUwZDKXC-lhfBV9l_dHEMvxCVdV"
+    },
+    version: '1.0.0'
+};
